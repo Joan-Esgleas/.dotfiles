453ae90a139e9bb2d1a71882e906be8be07926dd
Joan <joan.esgleas@gmail.com>
2024-02-16 23:53:33 +0100

    Añadimos un script de setup

---
+++ setup_env.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+
+if command -v "starship" >/dev/null 2>&1; then
+    echo "starship existe"
+else
+    curl -sS https://starship.rs/install.sh | sh
+fi
+
+if command -v "stow" >/dev/null 2>&1; then
+    echo "stow existe"
+else
+if command -v apt >/dev/null 2>&1; then
+    echo "apt está instalado."
+    sudo apt install stow
+elif command -v pacman >/dev/null 2>&1; then
+    echo "pacman está instalado."
+    sudo pacman -S stow
+elif command -v dnf >/dev/null 2>&1; then
+    echo "dnf está instalado."
+    sudo dnf install stow
+else
+    echo "No se encontró un gestor de paquetes compatible."
+    exit 1
+fi
+fi
+
+stow --adopt -vt ~ */
